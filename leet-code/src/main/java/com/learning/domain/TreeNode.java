a80659ca194afe1e9af3706044278f22f4b4340a
wcy <s2288156@163.com>
2020-09-24 15:25:21 +0800

    [二叉搜索树中的众数] 数据结构创建

---
+++ leet-code/src/main/java/com/learning/domain/TreeNode.java
@@ -0,0 +1,16 @@
+package com.learning.domain;
+
+/**
+ * @author wcy
+ */
+public class TreeNode {
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
---
+++ leet-code/src/main/java/com/learning/easy/E501FindMode.java
@@ -0,0 +1,24 @@
+package com.learning.easy;
+
+/**
+ * Definition for a binary tree node.
+ * <pre>
+ * public class TreeNode {
+ *    int val;
+ *    TreeNode left;
+ *    TreeNode right;
+ *    TreeNode(int x) { val = x; }
+ * }
+ * </pre>
+ * <p>https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/</p>
+ *
+ * @author wcy
+ */
+public class E501FindMode {
+
+    public int[] findMode() {
+
+        return null;
+    }
+
+}

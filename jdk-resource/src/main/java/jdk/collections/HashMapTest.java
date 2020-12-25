0f54dde5d29e123cc1894d61b94ad1cc728d8e1e
wcy <s2288156@163.com>
2020-12-25 11:14:15 +0800

    jdk源码阅读-HashMapTest

---
+++ jdk-resource/src/main/java/jdk/collections/HashMapTest.java
@@ -0,0 +1,12 @@
+package jdk.collections;
+
+import java.util.HashMap;
+
+/**
+ * @author wcy
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<Object, Object> hashMap = new HashMap<>();
+    }
+}

69c94f21bc972f1595e6211cfd23794fc8d37ccd
wcy <s2288156@163.com>
2020-12-28 16:37:10 +0800

    dubbo-samples 消费者nacos注册

---
+++ dubbo-examples/consumer-nacos-example/src/main/java/com/example/consumer/controller/ConsumerController.java
@@ -0,0 +1,21 @@
+package com.example.consumer.controller;
+
+import com.example.provider.api.ProviderApi;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wcy
+ */
+@RestController
+public class ConsumerController {
+
+    @DubboReference
+    private ProviderApi providerApi;
+
+    @GetMapping("/say")
+    public String say() {
+        return providerApi.sayHello("consumer web invoke");
+    }
+}

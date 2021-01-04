503744d3c0d504870a6236816078a7edb5cd8085
wcy <s2288156@163.com>
2021-01-04 22:22:11 +0800

    E1046LastStoneWeight

--- leet-code/src/main/java/com/learning/easy/E1046LastStoneWeight.java
+++ leet-code/src/main/java/com/learning/easy/E1046LastStoneWeight.java
@@ -26,5 +26,22 @@ public class E1046LastStoneWeight {
 //        System.out.println(queue);
     }
 
+    public int run(int[] stones) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            queue.offer(stone);
+        }
+
+        while (queue.size() > 1) {
+            int a = queue.poll();
+            int b = queue.poll();
+
+            if (a > b) {
+                queue.offer(a - b);
+            }
+        }
+
+        return queue.isEmpty() ? 0 : queue.poll();
+    }
 
 }

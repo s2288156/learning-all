7f399183c0f9e9a26fb6526b2398d08d43278be7
wcy <s2288156@163.com>
2021-02-22 11:02:26 +0800

    [插入排序] 插入排序优化参数命名

--- leet-code/src/main/java/com/learning/algoithm/sort/InsertSort.java
+++ leet-code/src/main/java/com/learning/algoithm/sort/InsertSort.java
@@ -22,7 +22,9 @@ public class InsertSort implements Sort<Integer> {
         int unSortedLength = elements.length - 1;
 
         for (int i = 0; i < unSortedLength; i++) {
+            // 未排序区的第一个元素
             Integer unSort = elements[sortedLength];
+            // 与已排序元素比较
             for (int j = 0; j < sortedLength; j++) {
                 // 1. 已排序区：如果unSort比所有的都大，则排在最后一位，直接完成本轮比对即可
                 // 2. 已排序区：unSort小于此位数，则排在此位前
@@ -42,13 +44,13 @@ public class InsertSort implements Sort<Integer> {
     /**
      * 将数组从指定位置开始，向右移动1位
      *
-     * @param sortedLength              已排序区的元素数量
-     * @param fromStartMoveElementIndex 开始移动的元素位置
+     * @param endMoveElementIndex   结束移动的元素数量
+     * @param startMoveElementIndex 开始移动的元素位置
      * @param elements                  待操作的数组
      */
-    private void rightMoveOnePlace(int sortedLength, int fromStartMoveElementIndex, Integer[] elements) {
-        for (int k = 0; k < sortedLength - fromStartMoveElementIndex; k++) {
-            elements[sortedLength - k] = elements[sortedLength - k - 1];
+    private void rightMoveOnePlace(int endMoveElementIndex, int startMoveElementIndex, Integer[] elements) {
+        for (int k = 0; k < endMoveElementIndex - startMoveElementIndex; k++) {
+            elements[endMoveElementIndex - k] = elements[endMoveElementIndex - k - 1];
         }
     }
 

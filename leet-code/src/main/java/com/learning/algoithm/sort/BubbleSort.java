3abb8687ecfe92f8630ea0947b24305183dba10b
wcy <s2288156@163.com>
2021-02-23 17:46:53 +0800

    coding

--- leet-code/src/main/java/com/learning/algoithm/sort/BubbleSort.java
+++ leet-code/src/main/java/com/learning/algoithm/sort/BubbleSort.java
@@ -15,16 +15,14 @@ public class BubbleSort implements Sort<Integer> {
     public Integer[] sort() {
         int length = elements.length;
         for (int i = 0; i < length - 1; i++) {
-            int index = 0;
             for (int j = 0; j < length - i - 1; j++) {
                 // 当前大于下一位，则互换位置，继续向后比较
-                if (elements[index] > elements[index + 1]) {
-                    Integer temp = elements[index];
-                    elements[index] = elements[index + 1];
-                    elements[index + 1] = temp;
+                if (elements[j] > elements[j + 1]) {
+                    Integer temp = elements[j];
+                    elements[j] = elements[j + 1];
+                    elements[j + 1] = temp;
                 }
                 // 当前小于或等于下一位，从下一位开始往后比较
-                index++;
             }
         }
         return elements;

e4d5bbbe64dc56cea14be8e03343b219966f3dd0
wcy <s2288156@163.com>
2021-02-28 09:52:14 +0800

    基于spring cloud stream rocketmq的实现

---
+++ mq-example/rocketmq-consumer/src/main/java/com/asktao/rocketmq/consumer/ReceiveService.java
@@ -0,0 +1,16 @@
+package com.asktao.rocketmq.consumer;
+
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wcy
+ */
+@Service
+public class ReceiveService {
+
+    @StreamListener("input")
+    public void receiveInput(String receiveMsg) {
+        System.out.println(">>>>>>> " + receiveMsg);
+    }
+}

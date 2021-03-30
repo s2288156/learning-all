a7c229098aab8794267103279f8894acbee71c8e
wcy <s2288156@163.com>
2021-03-30 13:52:52 +0800

    netty DiscardServer

--- netty-examples/sample/src/main/java/com/netty/sample/discard/DiscardServerHandler.java
+++ netty-examples/sample/src/main/java/com/netty/sample/discard/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package com.netty.sample.discard;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * @author wcy
@@ -11,7 +12,14 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ((ByteBuf) msg).release();
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     @Override

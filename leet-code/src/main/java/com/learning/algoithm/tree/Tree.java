899e2f4c78745d6d5952f3fea0156b87da0ac630
wcy <s2288156@163.com>
2021-04-16 11:17:52 +0800

    定义普通树结构

---
+++ leet-code/src/main/java/com/learning/algoithm/tree/Tree.java
@@ -0,0 +1,16 @@
+package com.learning.algoithm.tree;
+
+import lombok.Data;
+
+/**
+ * @author wcy
+ */
+@Data
+public class Tree {
+
+    private Integer data;
+
+    private Tree left;
+
+    private Tree right;
+}

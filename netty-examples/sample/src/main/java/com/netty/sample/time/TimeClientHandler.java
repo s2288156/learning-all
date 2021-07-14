5ab9451f67bf5bad195de9d31d746c9026f13113
武春阳 <s2288156@163.com>
2021-07-14 17:50:43 +0800

    Stream-based transport First Solution

--- netty-examples/sample/src/main/java/com/netty/sample/time/TimeClientHandler.java
+++ netty-examples/sample/src/main/java/com/netty/sample/time/TimeClientHandler.java
@@ -11,15 +11,31 @@ import java.util.Date;
  */
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
 
+    private ByteBuf buf;
+
+    /**
+     * handlerAdded() / handlerRemoved()两个生命周期监听方法，可以执行任意两个初始化任务，只要不会阻塞太长时间
+     */
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        buf = ctx.alloc().buffer(4);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        buf.release();
+        buf = null;
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf m = (ByteBuf) msg;
-        try{
-            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+        buf.writeBytes(m);
+        m.release();
+        if (buf.readableBytes() >= 4) {
+            long currentTimeMillis = (buf.readUnsignedInt() - 2208988800L) * 1000L;
             System.out.println(new Date(currentTimeMillis));
             ctx.close();
-        } finally {
-            m.release();
         }
     }
 

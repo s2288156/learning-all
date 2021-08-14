db1c913e1be50aa2148a8a5ad65a08aeab68bc27
武春阳 <s2288156@163.com>
2021-08-14 17:15:24 +0800

    委托模式

---
+++ design-mode/src/main/java/com/design/delegate/Duck.java
@@ -0,0 +1,27 @@
+package com.design.delegate;
+
+/**
+ * @author wuyang
+ */
+public abstract class Duck {
+    private FlyBehavior flyBehavior;
+
+    private QuackBehavior quackBehavior;
+
+    public Duck() {
+    }
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+}
---
+++ design-mode/src/main/java/com/design/delegate/QuackBehavior.java
@@ -0,0 +1,8 @@
+package com.design.delegate;
+
+/**
+ * @author wuyang
+ */
+public interface QuackBehavior {
+    void quack();
+}

6f9dd53b0cd5e09e54e0d1229f881e3126d936ab
WuChunYang <s2288156@163.com>
2021-12-30 16:24:50 +0800

    flink-example: WordCount批处理

--- flink-example/src/main/java/com/flink/wc/WordCount.java
+++ flink-example/src/main/java/com/flink/wc/WordCount.java
@@ -24,7 +24,7 @@ public class WordCount {
 
     public static class WordContMap implements FlatMapFunction<String, Tuple2<String, Integer>> {
         @Override
-        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) {
             String[] words = s.split(" ");
             for (String word : words) {
                 collector.collect(new Tuple2<>(word, 1));

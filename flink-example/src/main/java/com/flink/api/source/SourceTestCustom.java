b041d931d29c7fb8e3c3ded8cd17750f9020d330
WuChunYang <s2288156@163.com>
2022-01-04 10:02:09 +0800

    flink-example:Source API: SourceTestCustom keyBy计算max

--- flink-example/src/main/java/com/flink/api/source/SourceTestCustom.java
+++ flink-example/src/main/java/com/flink/api/source/SourceTestCustom.java
@@ -3,6 +3,7 @@ package com.flink.api.source;
 import com.flink.api.beans.SensorReading;
 import com.flink.api.source.custom.SensorSourceTest;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -11,8 +12,11 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class SourceTestCustom {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStream<SensorReading> inputSource = env.addSource(new SensorSourceTest());
-        inputSource.print();
+        DataStream<SensorReading> inputStream = env.addSource(new SensorSourceTest());
+        SingleOutputStreamOperator<SensorReading> outputSteam = inputStream.keyBy(SensorReading::getId)
+                .max("temperature");
+
+        outputSteam.print();
 
         env.execute();
     }
--- flink-example/src/main/java/com/flink/api/source/custom/SensorSourceTest.java
+++ flink-example/src/main/java/com/flink/api/source/custom/SensorSourceTest.java
@@ -1,7 +1,6 @@
 package com.flink.api.source.custom;
 
 import com.flink.api.beans.SensorReading;
-import org.apache.commons.lang3.RandomUtils;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 import java.util.HashMap;
@@ -13,17 +12,18 @@ import java.util.Random;
  */
 public class SensorSourceTest implements SourceFunction<SensorReading> {
     private boolean running = true;
+
     @Override
     public void run(SourceContext<SensorReading> ctx) throws Exception {
         while (running) {
             Random random = new Random();
             Map<String, Double> initSensorMap = new HashMap<>(16);
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 3; i++) {
                 initSensorMap.put("sid_" + i, random.nextGaussian() + 30);
             }
 
             while (running) {
-                initSensorMap.forEach((k,v) -> {
+                initSensorMap.forEach((k, v) -> {
                     ctx.collect(new SensorReading(k, v + random.nextGaussian(), System.currentTimeMillis()));
                 });
                 Thread.sleep(2000);

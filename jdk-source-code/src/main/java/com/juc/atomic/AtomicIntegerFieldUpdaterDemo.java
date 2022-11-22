89aea550be171238ce6ed4c99eb979c61aa49e1c
JackWu <s2288156@163.com>
2022-11-22 19:37:46 +0800

    AtomicIntegerFieldUpdater 保证线程操作安全

--- jdk-source-code/src/main/java/com/juc/atomic/AtomicIntegerFieldUpdaterDemo.java
+++ jdk-source-code/src/main/java/com/juc/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -4,14 +4,17 @@ import com.utils.SleepUtils;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
 @Slf4j
-public class AtomicIntegerFieldUpdater {
+public class AtomicIntegerFieldUpdaterDemo {
     public static void main(String[] args) {
         Account account = new Account();
         for (int i = 0; i < 10; i++) {
             new Thread(() -> {
                 for (int j = 0; j < 1000; j++) {
-                    account.increment();
+                    // account.increment();
+                    account.atomicIncrement();
                 }
             }, "t" + i).start();
         }
@@ -24,14 +27,20 @@ public class AtomicIntegerFieldUpdater {
 @Data
 class Account {
     private String name;
-    private Integer total;
+    private volatile int total;
 
     public Account() {
         this.name = "aka";
         this.total = 0;
     }
 
-    public synchronized void increment() {
-        total++;
+    // public synchronized void increment() {
+    //     total++;
+    // }
+
+    AtomicIntegerFieldUpdater<Account> fieldUpdater = AtomicIntegerFieldUpdater.newUpdater(Account.class, "total");
+
+    public void atomicIncrement() {
+        fieldUpdater.incrementAndGet(this);
     }
 }

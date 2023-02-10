3ee70f49d786b64e88a6acec0fbe77197e99a41b
JackWu <s2288156@163.com>
2023-02-10 15:34:52 +0800

    offer 35: 复杂链表的复制

--- leet-code/src/main/java/com/leetcode/offer/Offer_35.java
+++ leet-code/src/main/java/com/leetcode/offer/Offer_35.java
@@ -1,15 +1,28 @@
 package com.leetcode.offer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
  */
 public class Offer_35 {
 
+    Map<Node, Node> nodeCache = new HashMap<>();
 
     public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
 
+        if (!nodeCache.containsKey(head)) {
+            Node newHead = new Node(head.val);
+            nodeCache.put(head, newHead);
+            newHead.next = copyRandomList(head.next);
+            newHead.random = copyRandomList(head.random);
+        }
 
-        return null;
+        return nodeCache.get(head);
     }
 
 

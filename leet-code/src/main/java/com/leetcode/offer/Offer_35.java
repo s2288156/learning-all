c04b27431383494c880ddeb092efa2002c2407e7
JackWu <s2288156@163.com>
2023-02-10 15:11:21 +0800

    offer 35: 复杂链表的复制

---
+++ leet-code/src/main/java/com/leetcode/offer/Offer_35.java
@@ -0,0 +1,28 @@
+package com.leetcode.offer;
+
+/**
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ */
+public class Offer_35 {
+
+
+    public Node copyRandomList(Node head) {
+
+
+        return null;
+    }
+
+
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}

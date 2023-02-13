eed955b3cf57824ad65f21022b40083358cc33b9
JackWu <s2288156@163.com>
2023-02-13 17:46:48 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

---
+++ leet-code/src/main/java/com/leetcode/offer/MissNumber.java
@@ -0,0 +1,20 @@
+package com.leetcode.offer;
+
+/**
+ * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ *
+ * @author Wu.Chunyang
+ */
+public class MissNumber {
+
+    public int missingNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        // 如果所有下标和值都相等，说明缺失的是最后一位
+        return nums.length;
+    }
+}

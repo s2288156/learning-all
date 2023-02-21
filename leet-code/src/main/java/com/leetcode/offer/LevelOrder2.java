2904da64c98e382a413530f364408a42795dc274
JackWu <s2288156@163.com>
2023-02-21 10:07:31 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II

---
+++ leet-code/src/main/java/com/leetcode/offer/LevelOrder2.java
@@ -0,0 +1,43 @@
+package com.leetcode.offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 32 - II. 从上到下打印二叉树 II
+ * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ *
+ * @author Wu.Chunyang
+ */
+public class LevelOrder2 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> req = new ArrayList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            req.add(temp);
+        }
+        return req;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}

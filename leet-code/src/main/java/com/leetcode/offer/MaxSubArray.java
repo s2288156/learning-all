bbeebcae95ef8338eeb8c128a525cf5181392029
JackWu <s2288156@163.com>
2023-02-28 17:43:30 +0800

    剑指 Offer 42. 连续子数组的最大和

--- leet-code/src/main/java/com/leetcode/offer/MaxSubArray.java
+++ leet-code/src/main/java/com/leetcode/offer/MaxSubArray.java
@@ -1,5 +1,8 @@
 package com.leetcode.offer;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * 剑指 Offer 42. 连续子数组的最大和
  * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
@@ -8,11 +11,27 @@ package com.leetcode.offer;
  * @author Wu.Chunyang
  */
 public class MaxSubArray {
+
     public int maxSubArray(int[] nums) {
-        int i = nums.length-1;
-        /**
-         * dp[i] = dp[i-1] + nums[i-1]
-         */
-        return -1;
+        int[] sumNums = new int[nums.length];
+        sumNums[0] = nums[0];
+        int max = sumNums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (sumNums[i - 1] > 0) {
+                sumNums[i] = sumNums[i - 1] + nums[i];
+            } else {
+                sumNums[i] = nums[i];
+            }
+            max = Math.max(max, sumNums[i]);
+        }
+        return max;
     }
+
+    @Test
+    void test_case1() {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        MaxSubArray maxSubArray = new MaxSubArray();
+        Assertions.assertEquals(6, maxSubArray.maxSubArray(nums));
+    }
+
 }

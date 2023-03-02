593b60a446b19d5381c7becff805fdeff6aa3adf
JackWu <s2288156@163.com>
2023-03-02 14:08:45 +0800

    剑指 Offer 46. 把数字翻译成字符串

---
+++ leet-code/src/main/java/com/leetcode/offer/TranslateNum.java
@@ -0,0 +1,45 @@
+package com.leetcode.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 剑指 Offer 46. 把数字翻译成字符串
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。
+ * 一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+ *
+ * @author Wu.Chunyang
+ */
+public class TranslateNum {
+    public int translateNum(int num) {
+        int[] nums = String.valueOf(num).chars().map(ch -> Character.digit(ch, 10)).toArray();
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                dp[i] = 1;
+            } else if (i == 1) {
+                if (isDoubleNum(nums, i)) {
+                    dp[i] = 2;
+                } else {
+                    dp[i] = 1;
+                }
+            } else {
+                if (isDoubleNum(nums, i)) {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            }
+        }
+
+        return dp[nums.length - 1];
+    }
+
+    private boolean isDoubleNum(int[] nums, int i) {
+        return nums[i - 1] * 10 + nums[i] > 9 && nums[i - 1] * 10 + nums[i] < 26;
+    }
+
+    @Test
+    void test_case1() {
+        System.out.println(new TranslateNum().translateNum(25));
+    }
+}

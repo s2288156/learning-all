4a97d3ecdde979c1eeae3b87eff717237e50ff83
JackWu <s2288156@163.com>
2023-04-11 11:18:41 +0800

    IO server client demo

---
+++ netty-examples/sample/src/main/java/com/io/IOClient.java
@@ -0,0 +1,28 @@
+package com.io;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * @author Wu.Chunyang
+ */
+public class IOClient {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true) {
+                    socket.getOutputStream().write((new Date() + ": hello world!").getBytes());
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+}

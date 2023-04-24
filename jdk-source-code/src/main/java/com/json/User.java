0ad49805021a2198e4b80b4ff8220280f38452a6
JackWu <s2288156@163.com>
2023-04-24 09:13:35 +0800

    fix code warning

--- jdk-source-code/src/main/java/com/json/User.java
+++ jdk-source-code/src/main/java/com/json/User.java
@@ -4,11 +4,9 @@ package com.json;
  * @author Wu.Chunyang
  */
 public interface User {
-    void setUsername(String username);
 
-    String getUsername();
+    void setUsername(String username);
 
     void setAge(Integer age);
 
-    Integer getAge();
 }

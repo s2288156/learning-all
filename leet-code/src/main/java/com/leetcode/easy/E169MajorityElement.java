4a1064c041c311bdfdf3fbf56804f5b9d2f959a5
JackWu <s2288156@163.com>
2023-07-20 16:06:47 +0800

    feat(算法): ✅ MajorityElement
    投票算法,yyds

--- leet-code/src/main/java/com/leetcode/easy/E169MajorityElement.java
+++ leet-code/src/main/java/com/leetcode/easy/E169MajorityElement.java
@@ -9,12 +9,34 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class E169MajorityElement {
     public int majorityElement(int[] nums) {
+        // return hashTable(nums);
+        return vot(nums);
+    }
+
+    // 投票算法，由于众数大于n/2，则只会有众数最后会有投票保留
+    private int vot(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+
+    private int hashTable(int[] nums) {
         Map<Integer, Integer> counter = new HashMap<>();
         for (int num : nums) {
             counter.compute(num, (k, v) -> v == null ? 1 : v + 1);
         }
         AtomicInteger max = new AtomicInteger();
-        counter.forEach((k,v) -> {
+        counter.forEach((k, v) -> {
             if (max.get() == 0) {
                 max.set(k);
             } else if (counter.get(max.get()) < v) {

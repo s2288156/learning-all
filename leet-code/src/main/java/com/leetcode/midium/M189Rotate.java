876f3c53c182bab01d1bcf0d51e67122d625b848
JackWu <s2288156@163.com>
2023-07-26 09:57:21 +0800

    feat(算法): ✅ Rotate 数组反转

--- leet-code/src/main/java/com/leetcode/midium/M189Rotate.java
+++ leet-code/src/main/java/com/leetcode/midium/M189Rotate.java
@@ -9,12 +9,12 @@ import org.junit.jupiter.api.Test;
  * @author Wu.Chunyang
  */
 public class M189Rotate {
-    public static void main(String[] args) {
-        int length = 8, k = 3;
-        for (int i = 0; i < length; i++) {
-            System.out.println((k + i) % length);
-        }
-    }
+    // public static void main(String[] args) {
+    //     int length = 8, k = 3;
+    //     for (int i = 0; i < length; i++) {
+    //         System.out.println((k + i) % length);
+    //     }
+    // }
 
     public static void bubble(int[] nums, int k) {
         int length = nums.length;
@@ -53,6 +53,7 @@ public class M189Rotate {
         System.arraycopy(newArray, 0, nums, 0, length);
     }
 
+    // 环状替换
     public static void function3(int[] nums, int k) {
         int length = nums.length;
         int pre, next, x;
@@ -73,10 +74,29 @@ public class M189Rotate {
         }
     }
 
+    // 数组反转
+    public static void function4(int[] nums, int k) {
+        int length = nums.length;
+        reverse(nums, 0, length - 1);
+        reverse(nums, 0, k % length - 1);
+        reverse(nums, k % length, length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+
+
     @Test
     void test_1() {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        function3(nums, 3);
+        function4(nums, 3);
         int[] exceptedNums = {5, 6, 7, 1, 2, 3, 4};
         for (int i = 0; i < nums.length; i++) {
             Assertions.assertEquals(exceptedNums[i], nums[i]);
@@ -86,7 +106,7 @@ public class M189Rotate {
     @Test
     void test_2() {
         int[] nums = {1, 2, 3, 4};
-        bubble(nums, 2);
+        function4(nums, 2);
         int[] exceptedNums = {3, 4, 1, 2};
         for (int i = 0; i < nums.length; i++) {
             Assertions.assertEquals(exceptedNums[i], nums[i]);
@@ -96,7 +116,7 @@ public class M189Rotate {
     @Test
     void test_3() {
         int[] nums = {1, 2};
-        bubble(nums, 2);
+        function4(nums, 2);
         int[] exceptedNums = {1, 2};
         for (int i = 0; i < nums.length; i++) {
             Assertions.assertEquals(exceptedNums[i], nums[i]);

33e5646eb852a04d3e3bd574d56c4cd1eb0eed41
JackWu <s2288156@163.com>
2023-08-29 10:30:38 +0800

    delete code

--- dart-examples/dart-demo1/bin/dart_demo1.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_demo1/Spacecraft.dart';
-import 'package:dart_demo1/dart_demo1.dart' as dart_demo1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_demo1.calculate()}!');
-  Spacecraft spacecraft =  Spacecraft('X', DateTime.now());
-  spacecraft.describe();
-}
--- dart-examples/dart-demo1/test/dart_demo1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_demo1/dart_demo1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}

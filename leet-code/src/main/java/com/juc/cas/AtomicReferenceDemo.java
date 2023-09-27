b39ccb276ccc1b8c0454807e07fac625d9def1b1
JackWu <s2288156@163.com>
2023-09-27 13:40:28 +0800

    feat(demo): Atomic Reference demo

--- leet-code/src/main/java/com/juc/cas/AtomicReferenceDemo.java
+++ leet-code/src/main/java/com/juc/cas/AtomicReferenceDemo.java
@@ -13,16 +13,19 @@ import java.util.concurrent.atomic.AtomicReference;
 public class AtomicReferenceDemo {
     public static void main(String[] args) {
         User zhangSan = new User("zhang san", 30);
+        User zhangSan1 = new User("zhang san", 30);
         User liSi = new User("li si", 22);
 
         AtomicReference<User> user = new AtomicReference<>();
 
         user.compareAndSet(null, zhangSan);
-        log.info("{}", user.get());
-        user.get().setAge(19);
-        log.info("{}", zhangSan);
-        user.compareAndSet(zhangSan, liSi);
-        log.info("{}", user.get());
+        log.info("ref {}", user.get());
+        // user.get().setAge(19);
+        log.info("zhangSan {}, zhangSan1 {}", zhangSan, zhangSan1);
+        log.info("zhangSan.equals(zhangSan1) = {}", zhangSan.equals(zhangSan1));
+        log.info("zhangSan == zhangSan1: {}", zhangSan == zhangSan1);
+        user.compareAndSet(zhangSan1, liSi);
+        log.info("{} , {}", user.compareAndSet(zhangSan1, liSi), user.get());
     }
 }
 

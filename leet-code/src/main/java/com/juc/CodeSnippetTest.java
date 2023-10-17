8a679acd467b2f6e5ac8e4f93b6c2a144e6aa50d
JackWu <s2288156@163.com>
2023-10-17 16:45:48 +0800

    feat(demo): demo coding

--- leet-code/src/main/java/com/juc/CodeSnippetTest.java
+++ leet-code/src/main/java/com/juc/CodeSnippetTest.java
@@ -1,5 +1,7 @@
 package com.juc;
 
+import org.openjdk.jol.info.ClassLayout;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,7 +10,27 @@ import java.time.format.DateTimeFormatter;
  */
 public class CodeSnippetTest {
     public static void main(String[] args) {
-        LocalDateTime d = LocalDateTime.now();
-        System.out.println(d.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        // LocalDateTime d = LocalDateTime.now();
+        // System.out.println(d.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        Object o = new Object();
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        UserObj uo = new UserObj();
+        System.out.println(ClassLayout.parseInstance(uo).toPrintable());
+        UserObjMt uom = new UserObjMt();
+        System.out.println(ClassLayout.parseInstance(uom).toPrintable());
+    }
+}
+
+class UserObj {
+    String name;
+    int age;
+}
+
+class UserObjMt {
+    String name;
+    int age;
+
+    public void print() {
+        System.out.println("name: " + name + ", age: " + age);
     }
 }

fa9ffc18d6b41b11b05259c71ab7b4f2ef4f3ac2
Wu.Chunyang <extern.Chunyang.Wu@kuka.com>
2024-04-15 17:26:57 +0800

    tcp delimiter test

--- netty-examples/sample/src/main/java/com/netty/tcp/TcpServerInitializer.java
+++ netty-examples/sample/src/main/java/com/netty/tcp/TcpServerInitializer.java
@@ -7,6 +7,8 @@ import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
+import java.util.Arrays;
+
 /**
  * @author Wu.Chunyang
  */
@@ -14,12 +16,29 @@ public class TcpServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ch.pipeline()
-                .addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, true, Unpooled.wrappedBuffer("STX".getBytes())))
-                .addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, true, Unpooled.wrappedBuffer("ETX".getBytes())))
+                .addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, false, Unpooled.wrappedBuffer("STX".getBytes())))
+                .addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, false, Unpooled.wrappedBuffer("ETX".getBytes())))
                 // .addLast(new StringDecoder())
                 // .addLast(new TcpServerHandler())
                 .addLast(new TcpByteServerHandler())
                 .addLast(new StringEncoder());
         // .addLast(new DnsTcpServerHandler());
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static void main(String[] args) {
+        byte[] bytes = "0".getBytes();
+        System.out.println(bytesToHex(bytes));  // 输出：0A0B0C0D
     }
 }

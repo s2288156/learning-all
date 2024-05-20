6c862c3282c9f6c47cd2c7a74c24bb94ca169129
JackWu <s2288156@163.com>
2024-05-20 14:16:28 +0800

    ads dll jni jar

---
+++ spring-boot-examples/web-example/src/main/java/com/demo/ads/AdDemo.java
@@ -0,0 +1,11 @@
+package com.demo.ads;
+
+/**
+ * @author Jack Wu
+ */
+public class AdDemo {
+    public static void main(String[] args) {
+        // System.out.println(System.getProperty("java.library.path"));
+        System.loadLibrary("AdsToJava");
+    }
+}

11b27a770a5bbf4476e9a8991af018e00493a456
Wu.Chunyang <extern.Chunyang.Wu@kuka.com>
2025-12-26 17:29:10 +0800

    init auth service

---
+++ micro-services/auth-service/src/main/java/com/all/micro/auth/AuthApplication.java
@@ -0,0 +1,14 @@
+package com.all.micro.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wu.Chunyang
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}

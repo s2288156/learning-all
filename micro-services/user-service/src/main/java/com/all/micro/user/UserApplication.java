344efb0bdbf3898e5130364a860016afbd272c9e
Wu.Chunyang <extern.Chunyang.Wu@kuka.com>
2025-12-26 17:31:28 +0800

    init user service

---
+++ micro-services/user-service/src/main/java/com/all/micro/user/UserApplication.java
@@ -0,0 +1,14 @@
+package com.all.micro.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wu.Chunyang
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
